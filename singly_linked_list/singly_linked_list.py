75c72a4fc0f832d818e382b3ec6713654827c914
Dajuan Mcdonald <16410995+FIOpwK@users.noreply.github.com>
2020-08-20 23:37:08 -0400

    fixed get_max

--- singly_linked_list/singly_linked_list.py
+++ singly_linked_list/singly_linked_list.py
@@ -12,12 +12,12 @@ class LinkedList:
         self.size = 0
 
 
-##    def iter(self):
-##        current = self.tail
-##        while current:
-##            val = current.value
-##            current = current.next
-##            yield val
+    def iter(self):
+        current = self.tail
+        while current:
+            val = current.value
+            current = current.next
+            yield val
 
     def __str__(self):
         return str(value)
@@ -34,12 +34,12 @@ class LinkedList:
             
             self.tail = new_tail
         self.size += 1
-##
-##    def contains(self, value):
-##        for node in self.iter():
-##            if value == node:
-##                return True
-##        return False
+
+    def contains(self, value):
+        for node in self.iter():
+            if value == node:
+                return True
+        return False
 
     def remove_head(self):
         if self.head is None:
@@ -60,5 +60,16 @@ class LinkedList:
         
 
     def get_max(self):
-        pass
+        if self.size == 0:
+            return None
+        if self.size == 1:
+            return self.head.value
+        current_max = self.head.value
+        current_node = self.head
+        while current_node is not None:
+            if current_max < current_node.value:
+                current_max = current_node.value
+
+            current_node = current_node.next
+        return current_max
         
